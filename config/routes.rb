67fa3c106fceb80cee9d3a6250737f270e39a7f2
Teshager <teshager8922@gmail.com>
2020-06-29 16:21:26 -0600

    adds style to the form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
   resources :users, only: [:new, :create, :edit, :update]
-  post 'users/new', to: "uses#new"
+  post 'users/new', to: "users#new"
+
 end
