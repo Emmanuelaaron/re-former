8e0b0bfa9c8e7d60a72c63d3054c5de662ce3a84
Emmanuelaaron <emmanuelisabirye9@gmail.com>
2020-06-29 19:23:53 +0300

    Adds validation to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+  validates :username, :email, :password,
+            presence: true, length: { minimum: 3, maximum: 254 }
+  validates :password,
+            format: {
+              with: /^(?=.\d)(?=.([a-z]|[A-Z]))([\x20-\x7E]){6,40}$/,
+              multiline: true
+            }
+  validates :email, format: { with: /@/ },
+                    uniqueness: { case_sensitive: false }
+
+  before_save { email.down_case! }
 end
